c36850d7ee99a17e6195fc4d93bfb3f322464263
Nikhilhututu <nik87jain@gmail.com>
2019-08-03 17:20:43 +0530

    08/03/2019:17:20:43.73

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectID
 const dbname = "myDataBase"
-const url = "mongodb://localhost:27017"
+// const url = "mongodb://localhost:27017"
+const url = 'mongodb://127.0.0.1:27017' 
 const mongoOptions  = {useNewUrlParser: true}
 
 const state ={
